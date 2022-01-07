b0e174f02eb69da918138a2f56e1fe5fa7eba8c7
John Ouendag <jouendag@primagine.nl>
2022-01-07 21:41:22 +0100

    up

--- main.go
+++ main.go
@@ -8,4 +8,20 @@ import (
 
 func main() {
 	fmt.Println(pakje.PrintVar())
+
+	// Arrays
+	var tabel [4]string
+	tabel[0] = "Hoppa1"
+	tabel[1] = "Hoppa2"
+	tabel[2] = "Hoppa3"
+	tabel[3] = "Hoppa4"
+	fmt.Println(tabel)
+
+	tabel2 := [3]string{"aap", "noot", "mies"}
+	fmt.Println(tabel2)
+
+	tabel3 := [2][5]string{{"a", "A", "AA", "AAA", "AAA"}, {"b", "B", "BB", "BBB", "BBBB"}}
+	fmt.Println(tabel3)
+	fmt.Println(tabel3[1][3])
+
 }
